2eb4eaf44ba3078b2005db3c6755f354f82283d2
omoflop <29988852+omoflop@users.noreply.github.com>
2023-08-25 11:59:50 -0700

    Allowed referencing vanilla sound groups

--- src/main/java/dev/omo/resourcefulblocksounds/RBSData.java
+++ src/main/java/dev/omo/resourcefulblocksounds/RBSData.java
@@ -6,6 +6,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,15 @@ public final class RBSData {
 	public static final Map<Identifier, BlockSoundGroup> customSoundGroups = new HashMap<>();
 
 	public static void clear() {
+		for (Field field : BlockSoundGroup.class.getFields()) {
+			try {
+				if (field.getType() == BlockSoundGroup.class && field.get(null) instanceof BlockSoundGroup soundGroup) {
+					customSoundGroups.put(new Identifier("minecraft", field.getName().toLowerCase()), soundGroup);
+				}
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}
 		customSoundGroups.clear();
 		for (Block block : Registries.BLOCK) {
 			((BlockAccessor)block).resourcefulBlockSounds$setBlockSoundGroup(null);
